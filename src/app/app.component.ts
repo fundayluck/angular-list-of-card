262faeb89ed309e80ed86f97eea094f0d892b170
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-07 07:13:43 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./card/card.component";
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, CardComponent, CommonModule]
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: '../assets/images/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today.'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: '../assets/images/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain.'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: '../assets/images/biking.jpeg',
+      username: 'Biking12312',
+      content: 'I did some biking today.'
+    },
+
+  ]
 }
